552bf9039eff466b9cce033aed922bed64701873
xiangyi-zhou <zxyzhouxiangyi@gmail.com>
2023-07-14 20:20:12 +0100

    update callback function

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from scrape import *
+from scrape import Scrape
 import dash
 from dash import dcc, html, dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+import io
+import pandas as pd
+import base64
 
-######################################################################################################################
-# Creating app instance and designing layout #
-######################################################################################################################
+# Hacky way of allowing downloading
+global_df = None
 
 default_parameters = {
-    'academic_discipline': 'computer-science',
+    'academic_discipline': 'computer-sciences',
     'hours_type': 'full-time',
     'funding_type': 'international-students',
     'ordered_keywords': "Artificial, Machine, Robot, Automation, Simulation",
@@ -26,33 +28,49 @@ You provide a set of standard input parameters:
 - **hours_type**
 - **funding_type**
 
-in addition to two non-standard paramaters: 
-- **Keywords in Title**: A list of keywords to search for in a title which, if matched for, increase the normalised rating. 
-- **Keywords to Exclude**: A list of keywords to search for in a title which renders the rating of that job zero. E.g. if you really hate Data discipline, you would include: "Data"
+in addition to two non-standard parameters: 
+- **Keywords in Title**: A list of keywords to search for in a title which, if matched, increase the normalized rating. 
+- **Keywords to Exclude**: A list of keywords to search for in a title which renders the rating of that job zero. E.g., if you really hate Data discipline, you would include: "Data"
 
-The web scraper searches through all the PhD listings with those paramaters and returns all the listings ordered by the "rating" metric based on the ordered list of keywords.
+The web scraper searches through all the PhD listings with those parameters and returns all the listings ordered by the "rating" metric based on the ordered list of keywords.
 
 You can then download the full dataframe as an excel sheet for convenience. 
 
-**NOTE: Parsing through all job descriptions can take sometime. **
+**NOTE: Parsing through all job descriptions can take some time.**
 
 '''
 
-# create scrape instance
-scraper = scrape()
+######################################################################################################################
+# Creating app instance and designing layout #
+######################################################################################################################
 
-# better stylesheet
+
+# Create scrape instance
+scraper = Scrape()
+
+# Define dropdown options
+funding_type_options = [
+    {"label": "EU Students", "value": "eu-students"},
+    {"label": "International Students", "value": "international-students"},
+    {"label": "Self-funded Students", "value": "self-funded-students"},
+    {"label": "UK Students", "value": "uk-students"},
+]
+
+hours_type_options = [
+    {"label": "Full-time", "value": "full-time"},
+    {"label": "Part-time", "value": "part-time"},
+]
+
+# Set external stylesheets
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Create app
-app = dash.Dash(external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assign server instance
-server = app.server
-# Create layout
+# Define app layout
 app.layout = html.Div(children=[
     dcc.Markdown(heading),
-    dbc.Progress(id="progress", value=0, striped=True, animated=True),
+    dbc.Progress(id="progress", value=0, max=100, striped=True, animated=True),
     html.Div(children=[
         html.Div(children=[
             html.Div(children=[
@@ -62,12 +80,12 @@ app.layout = html.Div(children=[
             ], className="four columns"),
             html.Div(children=[
                 html.Label('Funding Type'),
-                dcc.Dropdown(['eu-students', 'international-students', 'self-funded-students',
-                             'uk-students'], value=default_parameters['funding_type'], id="funding_type"),
+                dcc.Dropdown(options=funding_type_options,
+                             value=default_parameters['funding_type'], id="funding_type"),
             ], className="four columns"),
             html.Div(children=[
                 html.Label('Hours Type'),
-                dcc.Dropdown(['full-time', 'part-time'],
+                dcc.Dropdown(options=hours_type_options,
                              value=default_parameters['hours_type'], id="hours_type"),
             ], className="four columns"),
         ], className="row", style={'padding': 10}),
@@ -96,7 +114,6 @@ app.layout = html.Div(children=[
             ],
             type="circle",
         ),
-        dcc.Interval(id="interval", interval=1000, n_intervals=0),
         html.Div(id='trigger', children=0, style=dict(display='none'))
     ])
 ])
@@ -108,8 +125,7 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
-    [Output("results", "children"),
-     Output('trigger', 'children')],
+    [Output("results", "children"), Output('trigger', 'children')],
     [Input("find_phds", "n_clicks")],
     [State("academic_discipline", "value"),
      State("hours_type", "value"),
@@ -119,7 +135,7 @@ app.layout = html.Div(children=[
 )
 def update_results(n_clicks, academic_discipline, hours_type, funding_type, ordered_keywords, exclude_keywords):
     # Don't bother updating if the page just opened
-    if n_clicks == 0 or n_clicks is None:
+    if n_clicks is None:
         raise PreventUpdate
 
     # Grab input
@@ -134,73 +150,72 @@ def update_results(n_clicks, academic_discipline, hours_type, funding_type, orde
         'ordered_keywords': ordered_keywords,
         'exclude_keywords': exclude_keywords,
     }
-    print(parameters)
 
     # Scrape based on parameters given
     df = scraper.get_scrape(parameters)
-    global global_df
     global_df = df
 
     # Column for data-table
-    columns = [
-        {"name": i, "id": i} for i in df.columns
-    ]
+    columns = [{"name": str(i), "id": str(i)} for i in df.columns]
 
     # Convert data to list of dictionaries
-    data = df.iloc[0:10, :].to_dict(orient='records')
+    data = df.to_dict(orient='records')
 
     # Results table div
-    results_div = html.Div(className="row", children=[
-        html.Div(className="twelve columns", children=[
-            dash_table.DataTable(id="data_output",
-                                 style_as_list_view=True,
-                                 style_header={
-                                     'backgroundColor': 'white',
-                                     'fontWeight': 'bold'
-                                 },
-                                 style_cell={
-                                     'overflow': 'hidden',
-                                     'textOverflow': 'ellipsis',
-                                                     'minWidth': '0px',
-                                                     'maxWidth': '180px'
-                                 },
-                                 data=data,
-                                 columns=columns
-                                 )])])
+    results_div = html.Div(className="container", children=[
+        html.Div(className="row", children=[
+            html.Div(className="twelve columns", children=[
+                dash_table.DataTable(
+                    id="data_output",
+                    style_as_list_view=True,
+                    style_header={
+                        'backgroundColor': 'white',
+                        'fontWeight': 'bold'
+                    },
+                    style_cell={
+                        'overflow': 'hidden',
+                        'textOverflow': 'ellipsis',
+                        'minWidth': '0px',
+                        'maxWidth': '180px'
+                    },
+                    data=data,
+                    columns=columns
+                )
+            ])
+        ]),
+        html.Br(),
+        html.A(
+            html.Button("Download Full Data as Excel File",
+                        className="button button-primary"),
+            id='download-link',
+            href="/download_excel/",
+            download="phd_data.xlsx"
+        )
+    ])
+
     # Div to output to results parent
     output_div = html.Div(className="row", children=[
         dcc.Markdown('''
-        
         ### Results: 
-                        
         '''),
-        results_div,
-        html.Br(),
-        html.A(html.Button("Download Full Data as Excel File", className="button button-primary"),
-               id='download-link',
-               href="/download_excel/"
-               )
+        results_div
     ])
 
     # Return output div as well as an output value to trigger div to start trigger callback
-    return ([output_div], 1)
+    return [output_div], 1
+
 
 # Callback which checks if button has been triggered already or not. Disables it if so.
-
-
 @app.callback(
     Output('find_phds', 'disabled'),
-    [Input('find_phds', 'n_clicks'),
-     Input('trigger', 'children')])
-def trigger_function(n_clicks, trigger):
-
+    [Input('find_phds', 'n_clicks'), Input('trigger', 'children')]
+)
+def trigger_function(n_clicks):
     # Grab the id of the element that triggered the callback
     context = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
 
     # If the button triggered the function
     if context == 'find_phds':
-
-        # Prevent false disabling at page load
         if n_clicks is None:
             return False
         elif n_clicks > 0:
@@ -208,8 +223,26 @@ def trigger_function(n_clicks, trigger):
         else:
             return False
     else:
-        return False    # If scrape completes and signals trigger again
+        return False  # If scrape completes and signals trigger again
+
+
+# Callback to handle the download link
+@app.callback(Output("download-link", "href"), [Input("download-link", "pathname")])
+def download_excel(pathname):
+    if pathname == "/download_excel/":
+        # Create Excel file in memory
+        output = io.BytesIO()
+        writer = pd.ExcelWriter(output, engine="xlsxwriter")
+        global_df.to_excel(writer, index=False)
+        writer.save()
+        output.seek(0)
+
+        # Return the download link
+        return f"data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{base64.b64encode(output.getvalue()).decode()}"
+    else:
+        raise PreventUpdate
 
 
 if __name__ == "__main__":
+    server = app.server
     app.run_server(debug=True)
--- scrape.py
+++ scrape.py
@@ -1,56 +1,102 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-from multiprocessing import Pool
-from functools import partial
+import re
 
 
-class scrape():
-
-    # Create base URL for all further scraping
+class Scrape:
+    @staticmethod
     def create_url(parameters):
-        # create base url for all further searches
         academic_discipline = parameters['academic_discipline']
         hours_type = parameters['hours_type']
         funding_type = parameters['funding_type']
         base_url = f"https://www.jobs.ac.uk/search/?activeFacet=hoursTypeFacet&sortOrder=1&pageSize=25&startIndex=1&academicDisciplineFacet%5B0%5D={academic_discipline}&jobTypeFacet%5B0%5D=phds&hoursTypeFacet%5B0%5D={hours_type}&fundingTypeFacet%5B0%5D={funding_type}"
         return base_url
 
-    # Rate phd based on parameters given
-    def rate_phd(p_title, p_soup, parameters):
-        # rate by keywords
-        # description = p_soup.select_one("#job-description.row-8").get_text()
+    @staticmethod
+    def rate_phd(data, parameters):
         title_keywords = parameters['ordered_keywords']
         exclude_keywords = parameters['exclude_keywords']
         total_keywords = len(title_keywords)
         title_keywords_present = []
         rating = 0
 
-        # Check for title keywords
         for index, keyword in enumerate(title_keywords):
-            if keyword in p_title:
+            if keyword in data['title']:
                 rating += total_keywords - index
                 title_keywords_present.append(keyword)
 
-        # Normalise rating
         rating = rating / sum(range(1, total_keywords + 1))
 
-        # Check for excluded keywords
         for keyword in exclude_keywords:
-            if keyword in p_title:
+            if keyword in data['title']:
                 rating = 0
                 break
 
         return rating, title_keywords_present
 
-    # Obtain details of the job (company, title, description etc.)
-    def get_phd_details(job, parameters):
-        pass
+    @staticmethod
+    def get_phd_details(url):
+        data = pd.DataFrame()
 
-    # Primary function for obtaining scraped data
+        total_phd_number_text = BeautifulSoup(requests.get(url).content, "html.parser") \
+            .select_one("#job-count h2.j-search-content__count.phd-branding").get_text(strip=True)
+        total_phd_number = int(re.sub(r'\D', '', total_phd_number_text))
+
+        print(f"[Collection] PhD data size: {total_phd_number}, collecting...")
+
+        total_page_number = (total_phd_number + 24) // 25
+
+        for i in range(1, total_page_number + 1):
+            index_start = (i - 1) * 25 + 1
+            page_url = f"{url}&startIndex={index_start}"
+            page = BeautifulSoup(requests.get(page_url).content, "html.parser")
+
+            # Extract data from the page
+            # Update the code to extract values for each column from the parsed HTML page
+            titles = [title.get_text(strip=True) for title in page.select(
+                "div.j-search-result__text a")]
+            urls = ["https://www.jobs.ac.uk" + url["href"]
+                    for url in page.select("div.j-search-result__text a")]
+            departments = [department.get_text(
+                strip=True) for department in page.select("div.j-search-result__department")]
+            departments = departments if departments else [None]
+            employers = [employer.get_text(strip=True) for employer in page.select(
+                "div.j-search-result__employer b")]
+            employers = employers if employers else [None]
+            locations = [location.get_text(strip=True).replace("Locations:", "").strip(
+            ) for location in page.select("div.j-search-result__text div:nth-child(4)")]
+            salary = [re.sub(r'\s+', ' ', salary.get_text(strip=True).replace("Salary:", "").strip())
+                      for salary in page.select("div.j-search-result__text div:nth-child(5)")]
+            post_dates = [post_date.get_text(strip=True).replace("Date Placed:", "").strip(
+            ) for post_date in page.select("div.j-search-result__text div:nth-child(6)")]
+            close_dates = [close_date.get_text(strip=True) for close_date in page.select(
+                "div.j-search-result__date-logos span.j-search-result__date--blue")]
+
+            page_data = pd.DataFrame({
+                "title": titles,
+                "employer": employers,
+                "department": departments,
+                "salary": salary,
+                "location": locations,
+                "post_date": post_dates,
+                "close_date": close_dates,
+                "url": urls
+            })
+
+            data = pd.concat([data, page_data], ignore_index=True)
+        print("Collection Done")
+        return data
+
     def get_scrape(self, parameters):
-        pass
+        base_url = self.create_url(parameters)
+        phd_data = self.get_phd_details(base_url)
+        rating_data = phd_data.apply(lambda row: self.rate_phd(
+            row, parameters), axis=1, result_type='expand')
+        output_data = pd.concat([phd_data, rating_data], axis=1)
+        output_data.to_csv("temp.csv")
+        return output_data
 
-        # For outputting to excel locally
-    def output_excel(df):
-        pass
+    @staticmethod
+    def output_excel(df, output_path):
+        df.to_excel(output_path, index=False)
