ecf8cd44c181265327e38455c5f99388e8b8ae8a
Deena Gendoo <DGendoo@users.noreply.github.com>
2023-07-21 15:07:52 +0100

    Update constant_manager.py
    Kept only relevant academic fields

--- constant_manager.py
+++ constant_manager.py
@@ -23,33 +23,13 @@ heading_job_page = '''
 academic_discipline_options = [
     {"label": "Agriculture, Food & Veterinary",
         "value": "agriculture-food-and-veterinary"},
-    {"label": "Architecture, Building & Planning",
-        "value": "architecture-building-and-planning"},
     {"label": "Biological Sciences", "value": "biological-sciences"},
-    {"label": "Business & Management Studies",
-        "value": "business-and-management-studies"},
     {"label": "Computer Sciences", "value": "computer-sciences"},
-    {"label": "Creative Arts & Design", "value": "creative-arts-and-design"},
-    {"label": "Economics", "value": "economics"},
-    {"label": "Education Studies", "value": "education-studies-inc-tefl"},
     {"label": "Engineering & Technology", "value": "engineering-and-technology"},
     {"label": "Health & Medical", "value": "health-and-medical"},
-    {"label": "Historical & Philosophical Studies",
-        "value": "historical-and-philosophical-studies"},
-    {"label": "Information Management & Librarianship",
-        "value": "information-management-and-librarianship"},
-    {"label": "Languages, Literature & Culture",
-        "value": "languages-literature-and-culture"},
-    {"label": "Law", "value": "law"},
     {"label": "Mathematics & Statistics", "value": "mathematics-and-statistics"},
-    {"label": "Media & Communications", "value": "media-and-communications"},
     {"label": "Physical & Environmental Sciences",
-        "value": "physical-and-environmental-sciences"},
-    {"label": "Politics & Government", "value": "politics-and-government"},
-    {"label": "Psychology", "value": "psychology"},
-    {"label": "Social Sciences & Social Care",
-        "value": "social-sciences-and-social-care"},
-    {"label": "Sport & Leisure", "value": "sport-and-leisure"}
+        "value": "physical-and-environmental-sciences"}
 ]
 
 default_parameters_phd_page = {
